86309fa7232d8cf7672f2070760e3479bf56e6e7
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2018-09-06 15:50:06 +0200

    forrester abstract server + svm classes

--- hpolib/container/client/ml/svm_benchmark.py
+++ hpolib/container/client/ml/svm_benchmark.py
@@ -1,6 +1,36 @@
 from hpolib.container.client.abstract_benchmark import AbstractBenchmarkClient
 
+class SvmOnMnist(AbstractBenchmarkClient):
+    def __init__(self):
+        self.bName = "SvmOnMnist"
+        self.setup() 
+
+
 class SvmOnVehicle(AbstractBenchmarkClient):
     def __init__(self):
         self.bName = "SvmOnVehicle"
+        self.setup() 
+
+
+class SvmOnCovertype(AbstractBenchmarkClient):
+    def __init__(self):
+        self.bName = "SvmOnCovertype"
+        self.setup() 
+
+
+class SvmOnLetter(AbstractBenchmarkClient):
+    def __init__(self):
+        self.bName = "SvmOnLetter"
+        self.setup() 
+
+
+class SvmOnAdult(AbstractBenchmarkClient):
+    def __init__(self):
+        self.bName = "SvmOnAdult"
+        self.setup() 
+
+
+class SvmOnHiggs(AbstractBenchmarkClient):
+    def __init__(self):
+        self.bName = "SvmOnHiggs"
         self.setup() 
--- hpolib/container/client/synthetic_functions/forrester.py
+++ hpolib/container/client/synthetic_functions/forrester.py
@@ -4,7 +4,3 @@ class Forrester(AbstractBenchmarkClient):
     def __init__(self):
         self.bName = "Forrester"
         self.setup()
-
-    def objective_function(self, x, fidelity=1, **kwargs):
-        kwargs["fidelity"] = fidelity
-        return super().objective_function(x, kwargs)
