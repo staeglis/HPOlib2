268fe947994b04966469e1d1ab536b034bb80127
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2018-10-08 23:01:06 +0200

    cartpole fix

--- hpolib/container/client/rl/cartpole.py
+++ hpolib/container/client/rl/cartpole.py
@@ -3,16 +3,16 @@ from hpolib.container.client.abstract_benchmark import AbstractBenchmarkClient
 class CartpoleBase(AbstractBenchmarkClient):
     def __init__(self):
         self.bName = "CartpoleBase"
-        self._setup()
+        self._setup(gpu=True)
 
 
 class CartpoleFull(AbstractBenchmarkClient):
     def __init__(self):
         self.bName = "CartpoleFull"
-        self._setup()
+        self._setup(gpu=True)
 
 
 class CartpoleReduced(AbstractBenchmarkClient):
     def __init__(self):
         self.bName = "CartpoleReduced"
-        self._setup()
+        self._setup(gpu=True)
