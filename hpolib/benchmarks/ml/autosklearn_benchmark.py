c6dfe525b4a7f74d162d5f3348ea9ac2cc7fb539
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2018-10-10 17:29:42 +0200

    allow newer auto-sklearn

--- hpolib/benchmarks/ml/autosklearn_benchmark.py
+++ hpolib/benchmarks/ml/autosklearn_benchmark.py
@@ -101,7 +101,7 @@ class AutoSklearnBenchmark(AbstractBenchmark):
                         'scipy>=0.14.1',
                         'scikit-learn>=0.19.0',
                         'pynisher==0.4.2',
-                        'auto-sklearn==0.3.0']
+                        'auto-sklearn>=0.3.0']
         dependencies = '\n'.join(dependencies)
         verify_packages(dependencies)
 
