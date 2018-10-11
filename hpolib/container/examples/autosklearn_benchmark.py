d469b9c6dd2cd68e30814439564d25ac6886f3c5
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2018-10-11 13:28:24 +0200

    autosklearn example

---
+++ hpolib/container/examples/autosklearn_benchmark.py
@@ -0,0 +1,32 @@
+import time
+import numpy as np
+
+try:
+    from hpolib.benchmarks.ml.autosklearn_benchmark import Covertype
+except ModuleNotFoundError:
+    from hpolib.container.client.ml.autosklearn_benchmark import Covertype
+
+import ConfigSpace
+
+
+# Perform random search on the Branin function
+
+b = Covertype()
+start = time.time()
+
+values = []
+
+cs = b.get_configuration_space()
+
+for i in range(2):
+    configuration = cs.sample_configuration()
+    # s = time.time()
+    rval = b.objective_function(configuration)
+    # print("Done, took %.2f s" % ((time.time() - s)))
+    loss = rval['function_value']
+    values.append(loss)
+
+print(np.min(values))
+
+print("Done, took totally %.2f s" % ((time.time() - start)))
+
