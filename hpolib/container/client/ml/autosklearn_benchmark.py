07fb13bed4ad71f21fb0fec388ee0dcf31e63636
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2018-10-16 16:50:05 +0200

    introduce class AutoSklearnBenchmark on client side for handling Enum StatusType

--- hpolib/container/client/ml/autosklearn_benchmark.py
+++ hpolib/container/client/ml/autosklearn_benchmark.py
@@ -2,76 +2,95 @@
 @author: Stefan Staeglich
 '''
 
+from smac.tae.execute_ta_run import StatusType
+
 from hpolib.container.client.abstract_benchmark import AbstractBenchmarkClient
 
 
-class Sick(AbstractBenchmarkClient):
+class AutoSklearnBenchmark(AbstractBenchmarkClient):
+    def objective_function(self, x, **kwargs):
+        result = super().objective_function(x, **kwargs)
+        result['status'] = eval(result['status'])
+        return result
+
+    def objective_function_test(self, x, **kwargs):
+        result = super().objective_function_test(x, **kwargs)
+        result['status'] = eval(result['status'])
+        return result
+
+    def test(self, *args, **kwargs):
+        result = super().objective_function_test(*args, **kwargs)
+        result['status'] = eval(result['status'])
+        return result
+
+
+class Sick(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "Sick"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class Splice(AbstractBenchmarkClient):
+class Splice(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "Splice"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class Adult(AbstractBenchmarkClient):
+class Adult(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "Adult"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class KROPT(AbstractBenchmarkClient):
+class KROPT(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "KROPT"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class MNIST(AbstractBenchmarkClient):
+class MNIST(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "MNIST"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class Quake(AbstractBenchmarkClient):
+class Quake(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "Quake"
         self._setup(gpu=True, imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class PC4(AbstractBenchmarkClient):
+class PC4(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "PC4"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class KDDCup09_appetency(AbstractBenchmarkClient):
+class KDDCup09_appetency(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "KDDCup09_appetency"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class MagicTelescope(AbstractBenchmarkClient):
+class MagicTelescope(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "MagicTelescope"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class OVABreast(AbstractBenchmarkClient):
+class OVABreast(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "OVABreast"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class Covertype(AbstractBenchmarkClient):
+class Covertype(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "Covertype"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
 
 
-class FBIS_WC(AbstractBenchmarkClient):
+class FBIS_WC(AutoSklearnBenchmark):
     def __init__(self, **kwargs):
         self.bName = "FBIS_WC"
         self._setup(imgName="AutoSklearnBenchmark", **kwargs)
