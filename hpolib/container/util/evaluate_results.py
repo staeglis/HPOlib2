76ec8b86c0c12230f2ed75ccf2f61fb5a579332d
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2018-11-22 15:32:23 +0100

    trajectory

--- hpolib/container/util/evaluate_results.py
+++ hpolib/container/util/evaluate_results.py
@@ -8,6 +8,8 @@ import json
 import os
 import sys
 
+from merge_test_performance_different_times import fill_trajectory
+
 
 class DataEvaluation:
     def __init__(self, path):
@@ -24,8 +26,6 @@ class DataEvaluation:
                 self.withContainer.append(self.loadFromFile(filewith))
                 filewithout = self.rootdir + d1 + "/" + d2 + "/native/run_1/traj_aclib2.json"
                 self.withContainer.append(self.loadFromFile(filewithout))
-        print(self.withContainer)
-
 
     def loadFromFile(self, filepath):
         """ Loads JSON data from a given path """
@@ -36,6 +36,27 @@ class DataEvaluation:
                 result.append(json.loads(line))
         return result
 
+    def trajectory(self, list):
+        """ Trajectory """
+
+        perfList = []
+        timeList = []
+
+        for l1 in list:
+            perf = []
+            time = []
+            for l2 in l1:
+                perf.append(l2["cost"])
+                time.append(l2["cpu_time"])
+            perfList.append(perf)
+            timeList.append(time)
+        return fill_trajectory(perfList, timeList)
+
+    def run(self):
+        self.parseData()
+        trajContainer = self.trajectory(self.withContainer)
+        trajNative = self.trajectory(self.withoutContainer)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -44,4 +65,4 @@ if __name__ == "__main__":
 
     path = sys.argv[1]
     de = DataEvaluation(path)
-    de.parseData()
+    de.run()
