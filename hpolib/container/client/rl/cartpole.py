d90abdcb908624c120bc058a5d704bb411f1a8c2
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2019-01-09 14:23:28 +0100

    No gpu usage in cartpole

--- hpolib/container/client/rl/cartpole.py
+++ hpolib/container/client/rl/cartpole.py
@@ -8,10 +8,10 @@ from hpolib.container.client.abstract_benchmark import AbstractBenchmarkClient
 class CartpoleFull(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         self.bName = "CartpoleFull"
-        self._setup(gpu=True, imgName="CartpoleBase", **kwargs)
+        self._setup(imgName="CartpoleBase", **kwargs)
 
 
 class CartpoleReduced(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         self.bName = "CartpoleReduced"
-        self._setup(gpu=True, imgName="CartpoleBase", **kwargs)
+        self._setup(imgName="CartpoleBase", **kwargs)
