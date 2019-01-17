1531f890cfff7d608e063dc2419c99cb473e1afb
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2019-01-17 15:40:45 +0100

    fcnet task_id

--- hpolib/container/client/ml/fcnet_classification.py
+++ hpolib/container/client/ml/fcnet_classification.py
@@ -6,6 +6,8 @@ from hpolib.container.client.abstract_benchmark import AbstractBenchmarkClient
 
 
 class ClassificationNeuralNetwork(AbstractBenchmarkClient):
-    def __init__(self, **kwargs):
+    def __init__(self, task_id, **kwargs):
         self.bName = "ClassificationNeuralNetwork"
+        # Add task_id to kwargs
+        kwargs["task_id"] = task_id
         self._setup(**kwargs)
