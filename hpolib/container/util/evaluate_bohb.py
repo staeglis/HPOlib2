4554503086724b0fe95c5d1155e2598b05b643b9
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2019-02-21 09:42:33 +0100

    bnn libgpuarray

--- hpolib/container/util/evaluate_bohb.py
+++ hpolib/container/util/evaluate_bohb.py
@@ -10,14 +10,18 @@ import matplotlib.pyplot
 import numpy
 import os
 import pandas
+import pickle
 import sys
 import typing
+
+import hpbandster.core.result as hpres
+import hpbandster.visualization as hpvis
 
 from plottingscripts.utils.merge_test_performance_different_times import fill_trajectory
 import plottingscripts.plotting.plot_methods as plot_methods
 
 
-class DataEvaluation:
+class BohbEvaluation:
     def __init__(self, path):
         self.withContainer = []
         self.withoutContainer = []
@@ -26,23 +30,19 @@ class DataEvaluation:
     def parseData(self):
         """ Iterates over the data directories and loads data """
 
-        for d1 in os.listdir(self.rootdir):
-            for d2 in os.listdir(self.rootdir + d1):
-                for runDir in os.listdir(self.rootdir + d1 + "/" + d2 + "/container/"):
-                    filewith = self.rootdir + d1 + "/" + d2 + "/container/" + runDir + "/traj_aclib2.json"
-                    self.withContainer.append(self.loadFromFile(filewith))
-                for runDir in os.listdir(self.rootdir + d1 + "/" + d2 + "/native/"):
-                    filewithout = self.rootdir + d1 + "/" + d2 + "/native/" + runDir + "/traj_aclib2.json"
-                    self.withoutContainer.append(self.loadFromFile(filewithout))
+        for d in os.listdir(self.rootdir):
+            filewith = self.rootdir + d + "/container-results.pkl"
+            self.withContainer.append(self.loadFromFile(filewith))
+            filewithout = self.rootdir + d + "/native-results.pkl"
+            self.withoutContainer.append(self.loadFromFile(filewithout))
 
     def loadFromFile(self, filepath):
         """ Loads JSON data from a given path """
 
         result = []
-        with open(filepath, encoding='utf-8-sig') as json_file:
-            for line in json_file:
-                result.append(json.loads(line))
-        return result
+        with open(filepath, "rb") as json_file:
+            result = pickle.load(json_file)
+            return result.get_incumbent_trajectory()
 
     def trajectory(self, dataList):
         """ Trajectory """
@@ -51,18 +51,21 @@ class DataEvaluation:
         timeList = []
 
         for l1 in dataList:
-            perf = []
-            time = []
-            for l2 in l1:
-                perf.append(l2["cost"])
-                time.append(l2["cpu_time"])
-            perfList.append(perf)
-            timeList.append(time)
+            dict = {}
+            dict[-1] = - 1
+            for t, p in sorted(zip(l1["times_finished"], l1["losses"])):
+                t = float(t)
+                if t not in dict:
+                    dict[t] = p
+                elif p > dict[t]:
+                    dict[t] = p
+            print(type(list(dict.keys())[0]))
+            perfList.append(list(dict.values()))
+            timeList.append([float(t) for t in dict.keys()])
         perfList, timeList = fill_trajectory(tuple(perfList), tuple(timeList))
         perfList = numpy.array(perfList).T
-        #perfList[perfList > 1] = 1
 
-        return perfList, timeList
+        return [p[1:-1] for p in perfList], timeList[1:-1]
 
     def run(self):
         self.parseData()
@@ -71,18 +74,20 @@ class DataEvaluation:
         fig = plot_methods.plot_optimization_trace_mult_exp(time_list=[trajContainer[1], trajNative[1]],
                                                             performance_list=[trajContainer[0], trajNative[0]],
                                                             name_list=["Container", "Native"],
-                                                            logx=True,
-                                                            #x_min=200,
+                                                            ylabel="loss",
+                                                            #agglomeration="median",
+                                                            #logx=True,
+                                                            #x_min=10,
                                                             #x_max=30000
                                                             )
-        matplotlib.pyplot.savefig("plot.png")
+        matplotlib.pyplot.savefig("bohb_plot.png")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: server.py <dataPath>")
+        print("Usage: evaluate_bohb.py <dataPath>")
         sys.exit()
 
     path = sys.argv[1]
-    de = DataEvaluation(path)
+    de = BohbEvaluation(path)
     de.run()
