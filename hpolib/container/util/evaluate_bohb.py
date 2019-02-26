33946620929470dc2a08c1af9b90f0a6ea1b5a01
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2019-02-26 11:11:34 +0100

    BOHB plot

--- hpolib/container/util/evaluate_bohb.py
+++ hpolib/container/util/evaluate_bohb.py
@@ -26,6 +26,7 @@ class BohbEvaluation:
         self.withContainer = []
         self.withoutContainer = []
         self.rootdir = path
+        self.b = 1.0
 
     def parseData(self):
         """ Iterates over the data directories and loads data """
@@ -56,37 +57,34 @@ class BohbEvaluation:
             for t, p, b in zip(l1["times_finished"], l1["losses"], l1["budgets"]):
                 if b not in timeDict:
                     timeDict[b] = [0, t]
-                    perfDict[b] = [100000, p]
+                    perfDict[b] = [1000, p]
                 else:
                     timeDict[b].append(t)
                     perfDict[b].append(p)
-            for b in l1["budgets"]:
-                perfDict[b][0] = perfDict[b][1]
-                perfList.append(perfDict[b])
-                timeList.append(timeDict[b])
+            perfDict[self.b][0] = perfDict[self.b][1]
+            perfList.append(perfDict[self.b])
+            timeList.append(timeDict[self.b])
         perfList, timeList = fill_trajectory(tuple(perfList), tuple(timeList))
         perfList = numpy.array(perfList).T
-        print(timeList)
-        print(perfList[0])
 
         return [p[1:-1] for p in perfList], timeList[1:-1]
 
     def run(self):
         self.parseData()
-        print(self.withContainer[1])
         trajContainer = self.trajectory(self.withContainer)
         trajNative = self.trajectory(self.withoutContainer)
         fig = plot_methods.plot_optimization_trace_mult_exp(time_list=[trajContainer[1], trajNative[1]],
                                                             performance_list=[trajContainer[0], trajNative[0]],
+                                                            title="BOHB on Cartpole, budget b = %0.1f" % (self.b),
                                                             name_list=["Container", "Native"],
                                                             ylabel="loss",
-                                                            agglomeration="median",
+                                                            agglomeration="mean",
                                                             #logx=True,
                                                             #x_min=1,
                                                             #x_max=30000,
                                                             y_max=3000
                                                             )
-        matplotlib.pyplot.savefig("bohb_plot.png")
+        matplotlib.pyplot.savefig("bohb_plot%0.1f.png" % (self.b))
 
 
 if __name__ == "__main__":
