31833c0d0e1f4bcfc9abee213ed39f27f4c956d1
Stefan Staeglich <staeglis@informatik.uni-freiburg.de>
2019-03-20 15:08:32 +0100

    GPU image for cartpole

--- hpolib/container/client/rl/cartpole.py
+++ hpolib/container/client/rl/cartpole.py
@@ -6,12 +6,18 @@ from hpolib.container.client.abstract_benchmark import AbstractBenchmarkClient
 
 
 class CartpoleFull(AbstractBenchmarkClient):
-    def __init__(self, **kwargs):
+    def __init__(self, gpu=False, **kwargs):
         self.bName = "CartpoleFull"
-        self._setup(imgName="CartpoleBase", **kwargs)
+        img = "CartpoleBase"
+        if gpu:
+            img = img + "GPU"
+        self._setup(imgName=img, gpu=gpu, **kwargs)
 
 
 class CartpoleReduced(AbstractBenchmarkClient):
-    def __init__(self, **kwargs):
+    def __init__(self, gpu=False, **kwargs):
         self.bName = "CartpoleReduced"
-        self._setup(imgName="CartpoleBase", **kwargs)
+        img = "CartpoleBase"
+        if gpu:
+            img = img + "GPU"
+        self._setup(imgName=img, gpu=gpu, **kwargs)
